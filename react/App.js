dd9355de6d9c277cb6cf6519f67528c1afe9acac
DESKTOP-K3NBJK4\home <dalong@sonnim.kr>
2017-01-01 17:21:01 +0900

    commit

--- react/App.js
+++ react/App.js
@@ -2,20 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Navbar from './navigation/Navbar';
-//import PostListContainer from './list/PostListContainer';
-import Detail from './detail/DetailViewContainer';
-import {Sample} from './detail/Sample';
+import PostListContainer from './list/PostListContainer';
 
 class App extends React.Component {
     render() {
         return (
             <div>
                 <Navbar />
-                {/*<PostListContainer />*/}
-                <div className="container">
-                    {/*<Detail/>*/}
-                    <Sample/>
-                </div>
+                <PostListContainer />
             </div>
         );
     }
