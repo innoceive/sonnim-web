44bf117e15e3ec584f251d5fdf40c7c9fa459eec
DESKTOP-K3NBJK4\home <dalong@sonnim.kr>
2017-01-01 18:33:34 +0900

    Label 추가

---
+++ react/common/component/LabelList.js
@@ -0,0 +1,22 @@
+/**
+ * Created by home on 2017-01-01.
+ */
+import React from 'react';
+import Label from './Label';
+
+class LabelList extends React.Component {
+    render() {
+        var labels = this.props.labels.map((label) => {
+            return <Label label={label} />
+        });
+
+        return (
+            <div className="label_list">
+                <ul>
+                    {labels}
+                </ul>
+            </div>
+        )
+    }
+}
+export default LabelList;
