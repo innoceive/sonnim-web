a3cc64df074a3a4e36b094e27b296b4ccfcddb6d
swacle <swacle@naver.com>
2017-01-03 21:46:54 +0900

    commit

--- react/App.js
+++ react/App.js
@@ -1,21 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import Navbar from './navigation/Navbar';
-//import PostListContainer from './list/PostListContainer';
-import Detail from './detail/DetailViewContainer';
-import {Sample} from './detail/Sample';
+<<<<<<< HEAD
+<<<<<<< HEAD
+import PostContainer from './main/PostContainer';
+=======
+import PostListContainer from './list/PostListContainer';
+>>>>>>> adebb5744449a038e928c0aa6123734dc38b5d25
+=======
+import PostListContainer from './list/PostListContainer';
+>>>>>>> adebb5744449a038e928c0aa6123734dc38b5d25
 
 class App extends React.Component {
     render() {
         return (
             <div>
                 <Navbar />
-                {/*<PostListContainer />*/}
-                <div className="container">
-                    {/*<Detail/>*/}
-                    <Sample/>
-                </div>
+<<<<<<< HEAD
+<<<<<<< HEAD
+                <PostContainer />
+=======
+                <PostListContainer />
+>>>>>>> adebb5744449a038e928c0aa6123734dc38b5d25
+=======
+                <PostListContainer />
+>>>>>>> adebb5744449a038e928c0aa6123734dc38b5d25
             </div>
         );
     }
