42517b4cdfbd5192906eff76e46d0f9394c4c047
Jeong YS <dionicis1@gmail.com>
2017-01-12 00:43:43 +0900

    디테일 페이지에 게스트 하우스에 있는 룸에대한 정보를 카드 형태로 만들어 리스트로 보여주는 작업 완료

--- react/detail/DetailViewContainer.js
+++ react/detail/DetailViewContainer.js
@@ -1,19 +1,21 @@
 import React, {Component} from 'react';
 import axios from 'axios';
 import {CreateMap} from './Map';
+import {tmpData} from './TempData';
+import {RoomListCard} from './DetailRoomListCard';
 
 class DetailViewContainer extends Component {
     constructor() {
         super();
         this.state = {
-            responseData: []
+            responseData: tmpData.data
         };
     }
 
     componentDidMount() {
         console.log("in did mount");
         // Optionally the request above could also be done as
-        axios.get('http://beta.api.sonnim.kr/guesthouse/detail/1')
+/*        axios.get('http://beta.api.sonnim.kr/guesthouse/detail/1')
             .then(response => {
                 //console.log(response);
                 this.setState({responseData: response.data.data});
@@ -21,20 +23,28 @@ class DetailViewContainer extends Component {
             })
             .catch(function (error) {
                 console.log(error);
-            });
-        // this.setState({responseData: tmpData.data});
+            });*/
+        //this.setState({responseData: tmpData.data});
     }
 
     CheckInTime(timeObject) {
-        if(typeof (timeObject.startTime) == 'undefined')
+        const time = timeObject;
+        if(typeof time.start == 'undefined'){
+            console.log("skip")
             return;
+        }
+        console.log("nonskip");
+        console.log(timeObject);
 
-        let times = (timeObject) => ({
-            startTime: timeObject.checkinStart.slice(0, 5),
-            endTime: timeObject.checkinEnd.slice(0, 5),
-            outTime: timeObject.checkout.slice(0, 5)
-        });
-        times();
+        var timeSlice = function(object){
+            var start = object.start.slice(0,5);
+            var end = object.end.slice(0,5);
+            var out = object.out.slice(0,5);
+            return ({startTime : start, endTime: end, outTime: out});
+        }
+
+        const times = timeSlice(timeObject);
+        console.log(times);
         return (
             <div>
                 <p className="guestHouseCheckInTime">체크인 : {times.startTime}
@@ -47,19 +57,19 @@ class DetailViewContainer extends Component {
     render() {
         const responseData = this.state.responseData;
         console.log("in render");
-        //console.log(responseData);
+        console.log(responseData);
         return (
             <div className="row">
-                <div className="col-md-8 col-md-offset-2">
+                <div className="col-md-8 col-md-offset-0">
                     <div className="thumbnail">
                         <img src={responseData.imageUrl} alt=""/>
                         <div className="caption">
                             <h3 className="guestHouseName">{responseData.name}</h3>
                             <p className="guestHouseAddress">{responseData.address}</p>
                             {this.CheckInTime({
-                                startTime: responseData.checkinStart,
-                                endTime: responseData.checkinEnd,
-                                outTime: responseData.checkout
+                                start: responseData.checkinStart,
+                                end: responseData.checkinEnd,
+                                out: responseData.checkout
                             })}
                         </div>
                         <CreateMap
@@ -69,6 +79,13 @@ class DetailViewContainer extends Component {
                                 name: responseData.name
                             }}/>
                     </div>
+                </div>
+                <div className="col-md-4">
+                    {responseData.rooms.map(function(room){
+                        return (<RoomListCard key={room.id}>
+                                {room}
+                            </RoomListCard>);
+                    })}
                 </div>
             </div>
         );
