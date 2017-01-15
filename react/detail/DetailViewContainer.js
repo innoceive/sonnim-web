4c083109015ffb411a16942ab55dd51fa94052a5
Jeong YS <dionicis1@gmail.com>
2017-01-15 23:41:38 +0900

    클래스내 state 값 초기화 코드 수정 naver 지도 컴포턴트 추가 및 위도 경도 검색 url 삽입(주소를 통한 위도경도 검색 api url)

--- react/detail/DetailViewContainer.js
+++ react/detail/DetailViewContainer.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import axios from 'axios';
-import {CreateMap} from './Map';
+import {CreateNaverMap} from './Map';
 import {tmpData} from './TempData';
 import {RoomListCard} from './DetailRoomListCard';
 
@@ -10,7 +10,22 @@ class DetailViewContainer extends Component {
         super(...arguments);
         this.state = {
             id: this.props.params.id,
-            responseData: tmpData.data
+            guestHouse : {
+              ghName : "",
+              address: "",
+              checkinStart: "",
+              checkinEnd: "",
+              checkout: "",
+              imageUrl: "",
+              phone: "",
+              rooms: [],
+              phone: "",
+              ghGps: {
+                long: 33.46,
+                lat: 126.31
+              }
+            },
+            socialNetworks : []
         };
     }
 
@@ -18,15 +33,36 @@ class DetailViewContainer extends Component {
         console.log("in did mount");
         // Optionally the request above could also be done as
         axios.get('http://beta.api.sonnim.kr/guesthouse/detail/'+this.state.id)
-            .then(response => {
-                //console.log(response);
-                this.setState({responseData: response.data.data});
-                console.log(this.state.responseData);
-            })
+            .then(function(response){
+              let ghdata = response.data.data;
+              this.setState({
+                guestHouse : {
+                  ghName : ghdata.name,
+                  address : ghdata.address,
+                  checkinStart : ghdata.checkinStart,
+                  checkinEnd : ghdata.checkinEnd,
+                  checkout : ghdata.checkout,
+                  imageUrl : ghdata.imageUrl,
+                  phone : ghdata.phone,
+                  rooms : ghdata.rooms,
+                  phone : ghdata.phone,
+                  ghGps : {
+                    long : ghdata.longitude,
+                    lat : ghdata.latitude
+                  }
+                }
+              });
+
+              let socialNet = this.state.socialNetworks.slice();
+              ghdata.socialNetworks.map(function(sn){
+                socialNet.push({type : sn.type, url : sn.value});
+              });
+              this.setState({socialNetworks : socialNet});
+            }
+            .bind(this))
             .catch(function (error) {
                 console.log(error);
             });
-        //this.setState({responseData: tmpData.data});
     }
 
     CheckInTime(timeObject) {
@@ -57,40 +93,34 @@ class DetailViewContainer extends Component {
     }
 
     render() {
-        const responseData = this.state.responseData;
-        console.log("in render");
-        console.log(responseData);
-        return (
-            <div className="row">
-                <div className="col-md-8 col-md-offset-0">
-                    <div className="thumbnail">
-                        <img src={responseData.imageUrl} alt=""/>
-                        <div className="caption">
-                            <h3 className="guestHouseName">{responseData.name}</h3>
-                            <p className="guestHouseAddress">{responseData.address}</p>
-                            {this.CheckInTime({
-                                start: responseData.checkinStart,
-                                end: responseData.checkinEnd,
-                                out: responseData.checkout
-                            })}
-                        </div>
-                        <CreateMap
-                            value={{
-                                lat: responseData.latitude,
-                                lng: responseData.longitude,
-                                name: responseData.name
-                            }}/>
-                    </div>
-                </div>
-                <div className="col-md-4">
-                    {responseData.rooms.map(function(room){
-                        return (<RoomListCard key={room.id}>
-                                {room}
-                            </RoomListCard>);
-                    })}
-                </div>
-            </div>
-        );
+      const ghData = this.state.guestHouse;
+      console.log(ghData);
+      return (
+          <div className="row">
+              <div className="col-md-8 col-md-offset-0">
+                  <div className="thumbnail">
+                      <img src={ghData.imageUrl} alt=""/>
+                      <div className="caption">
+                          <h3 className="guestHouseName">{ghData.name}</h3>
+                          <p className="guestHouseAddress">{ghData.address}</p>
+                          {this.CheckInTime({
+                              start: ghData.checkinStart,
+                              end: ghData.checkinEnd,
+                              out: ghData.checkout
+                          })}
+                      </div>
+                      <CreateNaverMap gps={ghData.ghGps} name={ghData.ghName}/>
+                  </div>
+              </div>
+              <div className="col-md-4">
+                  {ghData.rooms.map(function(room){
+                      return (<RoomListCard key={room.id}>
+                              {room}
+                          </RoomListCard>);
+                  })}
+              </div>
+          </div>
+      );
     }
 }
 
--- react/detail/Map.js
+++ react/detail/Map.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 
-export class CreateMap extends Component{
+export class CreateDaumMap extends Component{
     constructor(){
         super();
         this.state = {
@@ -53,4 +53,21 @@ function setInfoWindow(markerPos, props){
     });
 }
 
+export class CreateNaverMap extends Component{
+    render(){
+        return (
+            <div id="map" style={{height: '400px'}}></div>
+        );
+    }
 
+    componentDidUpdate(){
+      const gps = this.props.gps;
+      /*
+      * https://navermaps.github.io/maps.js/docs/tutorial-3-geocoder-geocoding.example.html
+      * 위에서 제공하는 주소 -> 좌표계를 이용하여 위도 경도 검색
+      */
+      const center = new naver.maps.LatLng(gps.lat, gps.long);
+      const map = new naver.maps.Map('map', {center: center, zoom: 13});
+      const marker = new naver.maps.Marker({position: center, map: map});
+    }
+}
