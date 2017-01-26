13f8f776f0d55c48986870446cd3ce120b4237dc
swacle <swacle@naver.com>
2017-01-26 21:54:17 +0900

    post css 적용

--- react/main/Post.js
+++ react/main/Post.js
@@ -5,22 +5,21 @@ import {Link} from 'react-router';
 class Post extends React.Component {
     render() {
         return (
-            <li className="post" >
-                <div className="post_image pull-left">
+            <li className="sn-post" >
+                <div className="sn-post-image pull-left">
                     <Link to={'/detail/'+this.props.post.id} className="post__link">
                         <img src={this.props.post.imageUrl} alt="test"/>
                     </Link>
                 </div>
-                <div className="post_contents">
-                    <div className="post_title">{this.props.post.name}</div>
-                    <div className="post_body">
-                        {this.props.post.description}
-                        {this.props.post.address}
-                    </div>
+                <div className="sn-post-content">
+                    <h4 className="sn-post-title">
+                        <a className="sn-post-name">{this.props.post.name}</a>
+                        <a className="sn-post-address">{this.props.post.address}</a>
+                    </h4>
                     <LabelList labels={this.props.post.labels} />
-                    <div className="map_image">
-                        <img src={this.props.post.mapImageUrl} alt="test"/>
-                    </div>
+                </div>
+                <div className="sn-post-map">
+                    <img src={this.props.post.mapImageUrl} alt="test"/>
                 </div>
             </li>
         )
